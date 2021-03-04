a953de5d61bb67e18127785274c43445ae1f9c73
cakeinsauce <minefrs@gmail.com>
2021-03-04 17:39:06 +0300

    Done with hw2 cache function; added tests

--- homework2/hw4.py
+++ homework2/hw4.py
@@ -18,8 +18,15 @@ val_2 = cache_func(*some)
 assert val_1 is val_2
 
 """
-from collections import Callable
+import functools
+from collections.abc import Callable
 
 
 def cache(func: Callable) -> Callable:
-    ...
+    """Return cached function's call."""
+
+    @functools.lru_cache
+    def cacheable_func(*args, **kwargs):
+        func(*args, **kwargs)
+
+    return cacheable_func
