b4e33f20eed6bf4724558ea906dec43e4d0d7615
cakeinsauce <minefrs@gmail.com>
2021-03-08 14:57:06 +0300

    Done with parameterized decorator cache / task1 homework3; added test

--- homework3/tests/test_task01.py
+++ homework3/tests/test_task01.py
@@ -1,12 +1,27 @@
-import functools
-from collections.abc import Callable
+import time
+
+import pytest
+
+from homework3.task01 import cache
 
 
-def cache(func: Callable) -> Callable:
-    """Return cached function's call."""
+@cache(times=2)
+def fun(a, b):
+    return (a ** b) ** 2
 
-    @functools.lru_cache
-    def cacheable_func(*args, **kwargs):
-        func(*args, **kwargs)
 
-    return cacheable_func
+@pytest.mark.parametrize(
+    "value",
+    [
+        (100, 40000),
+    ],
+)
+def test_cache(value: int):
+    # Cache decorator with cache's maxsize arg test.
+    first_time = time.time()
+    actual_result = fun(*value)
+    print("\nFirst call: {:s}".format(str(time.time() - first_time)))
+    second_time = time.time()
+    expected_result = fun(*value)
+    print("Second call: {:s}".format(str(time.time() - second_time)))
+    assert actual_result is expected_result
