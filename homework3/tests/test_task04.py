28b333696c6b559e79e921516cedf7786066a392
cakeinsauce <minefrs@gmail.com>
2021-03-09 00:25:36 +0300

    Initial commit task4 homework3

---
+++ homework3/tests/test_task04.py
@@ -0,0 +1,14 @@
+import pytest
+
+from homework3.task04 import is_armstrong
+
+
+@pytest.mark.parametrize(
+    ["value", "expected_value"],
+    [
+        (),
+    ],
+)
+def test_is_armstrong(value: int, expected_value: bool):
+    # Testing is_armstrong number.
+    pass
