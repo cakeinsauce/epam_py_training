569831f0e323e3f2e2e104802b222e200ee886c0
cakeinsauce <minefrs@gmail.com>
2021-03-25 20:34:51 +0300

    Done with hw6 task1; added tests

--- homework6/counter.py
+++ homework6/counter.py
@@ -11,18 +11,32 @@ reset_instances_counter - сбросить счетчик экземпляров
 
 
 def instances_counter(cls):
-    """Some code"""
-    return cls
+    """Class decorator to keep quantity of class instances."""
+
+    class Wrapper:
+        __instances_amt = 0
+
+        def __init__(self, *args, **kwargs):
+            Wrapper.__instances_amt += 1
+            self.wrap = cls(*args, **kwargs)
+
+        @classmethod
+        def get_created_instances(cls):
+            return cls.__instances_amt
+
+        @classmethod
+        def reset_instances_counter(cls):
+            before_reset, cls.__instances_amt = cls.__instances_amt, 0
+            return before_reset
+
+    return Wrapper
 
 
-@instances_counter
-class User:
-    pass
-
-
-if __name__ == "__main__":
-
-    User.get_created_instances()  # 0
-    user, _, _ = User(), User(), User()
-    user.get_created_instances()  # 3
-    user.reset_instances_counter()  # 3
+#
+#
+# if __name__ == "__main__":
+#
+#     User.get_created_instances()  # 0
+#     user, _, _ = User(), User(), User()
+#     user.get_created_instances()  # 3
+#     user.reset_instances_counter()  # 3
