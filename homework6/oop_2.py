40ecb5e89d9d5e1612cc473497196f34f977b7fb
cakeinsauce <minefrs@gmail.com>
2021-03-26 17:14:11 +0300

    Done with hw6 task2

--- homework6/oop_2.py
+++ homework6/oop_2.py
@@ -49,35 +49,144 @@ PEP8 соблюдать строго.
 К названием остальных переменных, классов и тд. подходить ответственно -
 давать логичные подходящие имена.
 """
-import datetime
+
+import datetime as dt
 from collections import defaultdict
 
-if __name__ == "__main__":
-    opp_teacher = Teacher("Daniil", "Shadrin")
-    advanced_python_teacher = Teacher("Aleksandr", "Smetanin")
 
-    lazy_student = Student("Roman", "Petrov")
-    good_student = Student("Lev", "Sokolov")
+class IsNotHomeworkInstanceError(Exception):
+    """Exception raised for errors in the input homework attribute."""
 
-    oop_hw = opp_teacher.create_homework("Learn OOP", 1)
-    docs_hw = opp_teacher.create_homework("Read docs", 5)
+    def __init__(self, homework: any, message="You gave a not Homework object"):
+        self.homework = homework
+        self.message = message
+        super().__init__(self.message)
 
-    result_1 = good_student.do_homework(oop_hw, "I have done this hw")
-    result_2 = good_student.do_homework(docs_hw, "I have done this hw too")
-    result_3 = lazy_student.do_homework(docs_hw, "done")
-    try:
-        result_4 = HomeworkResult(good_student, "fff", "Solution")
-    except Exception:
-        print("There was an exception here")
-    opp_teacher.check_homework(result_1)
-    temp_1 = opp_teacher.homework_done
+    def __str__(self):
+        return f"{type(self.homework)} --> {self.message}"
 
-    advanced_python_teacher.check_homework(result_1)
-    temp_2 = Teacher.homework_done
-    assert temp_1 == temp_2
 
-    opp_teacher.check_homework(result_2)
-    opp_teacher.check_homework(result_3)
+class DeadlineError(Exception):
+    """Exception raised for missed homework deadline."""
 
-    print(Teacher.homework_done[oop_hw])
-    Teacher.reset_results()
+    def __init__(self, homework, message="You are late"):
+        self.homework = homework
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"Deadline: {self.homework.created + self.homework.deadline}, Now: {dt.datetime.today()} --> {self.message}"
+
+
+class Person:
+    """Person class contains name of a person."""
+
+    def __init__(self, last_name: str, first_name: str):
+        self.last_name = last_name
+        self.first_name = first_name
+
+
+class Student(Person):
+    """Student class contains student methods for homework."""
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+    def do_homework(self, homework: "Homework", solution: str) -> "HomeworkResult":
+        """Return HomeworkResult object if it's not expired yet otherwise, DeadlineError is raised."""
+        if not homework.is_active():
+            raise DeadlineError(homework)
+        return HomeworkResult(homework, self, solution)
+
+
+class Teacher(Person):
+    """Teacher class contains teacher methods for homework."""
+
+    homework_done = defaultdict(list)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+    @staticmethod
+    def create_homework(text: str, days: int):
+        """Return created Homework object."""
+        return Homework(text, dt.timedelta(days=days))
+
+    @staticmethod
+    def check_homework(homework_result: "HomeworkResult") -> bool:
+        """Return True if homework solution is more than 5 characters and add homework_result to homework_done otherwise just return False."""
+
+        if len(homework_result.solution) <= 5:
+            return False
+
+        Teacher.homework_done[homework_result.homework].append(
+            homework_result
+        )  # Add homework result to done homeworks, group by Homework instance.
+        return True
+
+    @staticmethod
+    def reset_results(homework=None):
+        """If homework is None, clear homework_done dict otherwise, remove homework's results."""
+        if homework is None:
+            Teacher.homework_done.clear()
+        else:
+            Teacher.homework_done[homework] = []
+
+
+class Homework:
+    """Homework class contains attributes about a homework."""
+
+    def __init__(self, text: str, deadline: dt.timedelta):
+        self.text = text
+        self.deadline = deadline
+        self.created = dt.datetime.today()
+
+    def __str__(self):
+        return self.text
+
+    def is_active(self) -> bool:
+        """Return True if homework is not expired."""
+        return dt.datetime.today() < self.created + self.deadline
+
+
+class HomeworkResult:
+    """HomeworkResult contains done homeworks."""
+
+    def __init__(self, homework: Homework, author: "Student", solution: str):
+        if not isinstance(homework, Homework):
+            raise IsNotHomeworkInstanceError(homework)
+        self.homework = homework
+        self.solution = solution
+        self.author = author
+        self.created = dt.datetime.today()
+
+
+# if __name__ == "__main__":
+#     opp_teacher = Teacher("Daniil", "Shadrin")
+#     advanced_python_teacher = Teacher("Aleksandr", "Smetanin")
+#
+#     lazy_student = Student("Roman", "Petrov")
+#     good_student = Student("Lev", "Sokolov")
+#
+#     oop_hw = opp_teacher.create_homework("Learn OOP", 1)
+#     docs_hw = opp_teacher.create_homework("Read docs", 5)
+#
+#     result_1 = good_student.do_homework(oop_hw, "I have done this hw")
+#     result_2 = good_student.do_homework(docs_hw, "I have done this hw too")
+#     result_3 = lazy_student.do_homework(docs_hw, "done")
+#     try:
+#         result_4 = HomeworkResult(good_student, "fff", "Solution")
+#     except Exception:
+#         print("There was an exception here")
+#     opp_teacher.check_homework(result_1)
+#     temp_1 = opp_teacher.homework_done
+#
+#     advanced_python_teacher.check_homework(result_1)
+#     temp_2 = Teacher.homework_done
+#     assert temp_1 == temp_2
+#
+#     opp_teacher.check_homework(result_2)
+#     opp_teacher.check_homework(result_3)
+#
+#     print(Teacher.homework_done[oop_hw])
+#     Teacher.reset_results()
